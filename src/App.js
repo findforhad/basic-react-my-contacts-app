a71823f24c39f9f0a7d19cf8656962edd1e6650f
findforhad <forhadhossenbabu@gmail.com>
2019-03-06 23:42:23 +0600

    initiated the entire project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
+import ListContact from "./ListContacts";
 
-class ContactList extends React.Component {
-  render() {
-    const peoples = this.props.contacts;
-    return (
-        <ol>
-            {peoples.map(people => {
-                return <li>{people.name}</li>
-            })}
-        </ol>
-    )
+const contacts = [
+  {
+    id: "ryan",
+    name: "Ryan Florence",
+    email: "ryan@reacttraining.com",
+    avatarURL: "http://localhost:5001/ryan.jpg"
+  },
+  {
+    id: "michael",
+    name: "Michael Jackson",
+    email: "michael@reacttraining.com",
+    avatarURL: "http://localhost:5001/michael.jpg"
+  },
+  {
+    id: "tyler",
+    name: "Tyler McGinnis",
+    email: "tyler@reacttraining.com",
+    avatarURL: "http://localhost:5001/tyler.jpg"
   }
-}
+];
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <ContactList
-          contacts={[
-            { name: "Ryan" },
-            { name: "Tailer" },
-            { name: "Reachard" }
-          ]}
-        />
-        <ContactList
-          contacts={[{ name: "Babu" }, { name: "Forhad" }, { name: "Hossein" }]}
-        />
+      <div>
+        <ListContact contacts={contacts}/>
       </div>
     );
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ListContact extends React.Component {
+    render(){
+        console.log("Props", this.props)
+        return (
+            <ol>
+
+            </ol>
+        )
+    }
+}
+
+export default ListContact
