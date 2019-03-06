92fa8dbc6da80c0ae2c22830c0b88967a04f4051
findforhad <forhadhossenbabu@gmail.com>
2019-03-07 00:13:51 +0600

    converted ListContact to a stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,29 +1,25 @@
 import React from "react";
 
-class ListContact extends React.Component {
-  render() {
-    return (
-      <ol className="contact-list">
-        {this.props.contacts.map(contact => {
-          return (
-            <li key={contact.id} className="contact-list-item">
-              <div
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}
-              />
-              <div className="contact-details">
-                <p>{contact.name}</p>
-                <p>{contact.email}</p>
-              </div>
-              <button className="contact-remove" />
-            </li>
-          );
-        })}
-      </ol>
-    );
-  }
-}
+const ListContact = props => (
+  <ol className="contact-list">
+    {props.contacts.map(contact => {
+      return (
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}
+          />
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className="contact-remove" />
+        </li>
+      );
+    })}
+  </ol>
+);
 
 export default ListContact;
