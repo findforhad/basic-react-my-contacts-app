2bce7bd06e8439c95dec25bc76ba6e46f2b62a43
findforhad <forhadhossenbabu@gmail.com>
2019-03-07 00:06:28 +0600

    added contact details

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,9 +3,23 @@ import React from "react";
 class ListContact extends React.Component {
   render() {
     return (
-      <ol>
+      <ol className="contact-list">
         {this.props.contacts.map(contact => {
-          return <li key={contact.id}>{contact.name}</li>;
+          return (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button className="contact-remove" />
+            </li>
+          );
         })}
       </ol>
     );
