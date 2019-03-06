c905ec2c2ceb617677021934893e0ab719c27668
findforhad <forhadhossenbabu@gmail.com>
2019-03-07 00:46:24 +0600

    sets contacts array as state property of the app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from "react";
 import ListContact from "./ListContacts";
 
-const contacts = [
-  {
-    id: "ryan",
-    name: "Ryan Florence",
-    email: "ryan@reacttraining.com",
-    avatarURL: "http://localhost:5001/ryan.jpg"
-  },
-  {
-    id: "michael",
-    name: "Michael Jackson",
-    email: "michael@reacttraining.com",
-    avatarURL: "http://localhost:5001/michael.jpg"
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    email: "tyler@reacttraining.com",
-    avatarURL: "http://localhost:5001/tyler.jpg"
-  }
-];
-
 class App extends React.Component {
+  state = {
+    contacts: [
+      {
+        id: "ryan",
+        name: "Ryan Florence",
+        email: "ryan@reacttraining.com",
+        avatarURL: "http://localhost:5001/ryan.jpg"
+      },
+      {
+        id: "michael",
+        name: "Michael Jackson",
+        email: "michael@reacttraining.com",
+        avatarURL: "http://localhost:5001/michael.jpg"
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        email: "tyler@reacttraining.com",
+        avatarURL: "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  };
   render() {
     return (
       <div>
-        <ListContact contacts={contacts}/>
+        <ListContact contacts={this.state.contacts} />
       </div>
     );
   }
