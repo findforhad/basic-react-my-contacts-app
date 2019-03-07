6d0663f85d70ea6fda627de4eb9ee81a26670028
findforhad <forhadhossenbabu@gmail.com>
2019-03-08 01:37:34 +0600

    use API to fetch remote contacts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
+import { getAll, remove, create } from "./utils/ContactAPI";
 import ListContact from "./ListContacts";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    contacts: [
-      {
-        id: "ryan",
-        name: "Ryan Florence",
-        email: "ryan@reacttraining.com",
-        avatarURL: "http://localhost:5001/ryan.jpg"
-      },
-      {
-        id: "michael",
-        name: "Michael Jackson",
-        email: "michael@reacttraining.com",
-        avatarURL: "http://localhost:5001/michael.jpg"
-      },
-      {
-        id: "tyler",
-        name: "Tyler McGinnis",
-        email: "tyler@reacttraining.com",
-        avatarURL: "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
   };
+
+  async componentDidMount() {
+    await getAll().then(contacts => this.setState({ contacts }));
+  }
 
   removeContact = contact => {
     this.setState(oldState => ({
@@ -34,11 +20,10 @@ class App extends React.Component {
 
   render() {
     return (
-      
-        <ListContact
-          contacts={this.state.contacts}
-          removeContact={this.removeContact}
-        />
+      <ListContact
+        contacts={this.state.contacts}
+        removeContact={this.removeContact}
+      />
     );
   }
 }
